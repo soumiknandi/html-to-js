c31c711b221f5db58aad626738c4aa85b92bf076
Soumik Nandi <soumikn14@gmail.com>
2023-01-10 01:24:15 +0530

    Initial Commit

---
+++ day3/second/second.js
@@ -0,0 +1,3 @@
+function showAlert() {
+  alert("You pressed the button");
+}
---
+++ day3/third/third.js
@@ -0,0 +1,7 @@
+function handleClick() {
+  document.getElementById("msg").innerHTML = "You Clicked Me";
+}
+
+function handleDblClick() {
+  document.getElementById("msg").innerHTML = "You Double Clicked Me";
+}
